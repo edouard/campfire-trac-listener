74c6be1415462f9e7d41028d6aa493a35705f6b1
Ben Belchak <ben@needle.com>
2010-05-12 06:46:08 +0800

    Modified campfirelistener.py to support API tokens

--- campfirelistener/campfirelistener.py
+++ campfirelistener/campfirelistener.py
@@ -8,20 +8,20 @@ class CampfireListener(Component):
 
     def _sendText(self, ticketid, text):
         try:
-            c = pinder.Campfire('campfire_subdomain')
-            c.login('email','password')
-            room = c.find_room_by_name('room')
-            room.speak("Trac: ticket #%i (http://url.to.your.trac.install/projects/project_name/ticket/%i) %s" % (ticketid, ticketid, text))
+            c = pinder.Campfire('SUBDOMAIN', 'API TOKEN')
+            room = c.room('ROOM ID')
+            room.speak("Trac: ticket #%i (http://trac.YOURDOMAIN.com/projects/PROJECT/ticket/%i) %s" % (ticketid, ticketid, text))
 
         except:
-        print "Unexpected error:", sys.exc_info()[0]
+            print "Unexpected error:", sys.exc_info()[0]
             return
 
     def ticket_created(self, ticket):
         self._sendText(ticket.id, "\"%s\" created by %s." % (ticket.values['summary'][0:100], ticket.values['reporter']))
 
     def ticket_changed(self, ticket, comment, author, old_values):
-    self._sendText(ticket.id, "changed by %s, Comment: %s." % (author, comment[0:100]))
+        self._sendText(ticket.id, "changed by %s, Comment: %s." % (author, comment[0:100]))
 
     def ticket_deleted(self, ticket):
         self._sendText(ticket.id, "Ticket deleted")
+
